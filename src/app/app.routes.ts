3d8b585f549e236e43a749820f05bd9ac817d95b
kabricks <kabuyedouglas53@gmail.com>
2025-09-02 05:58:14 +0300

    remove announcements

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,13 +5,11 @@ import { Gallery } from './gallery/gallery';
 import { Rsvp } from './rsvp/rsvp';
 import { Contributions } from './contributions/contributions';
 import { Timeline } from './timeline/timeline';
-import { Announcements } from './announcements/announcements';
 
 export const routes: Routes = [
     { path: '', component: Home },
     { path: 'about', component: About },
     { path: 'timeline', component: Timeline },
-    { path: 'announcements', component: Announcements },
     { path: 'gallery', component: Gallery },
     { path: 'contributions', component: Contributions },
     { path: 'rsvp', component: Rsvp },
